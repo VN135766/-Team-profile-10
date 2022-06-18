f74fb9112c34c696211f27bba233033c77650047
Victor Nunez <VN135766@gmail.com>
2022-06-17 22:12:28 -0400

    passed a few test

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const Employee = require('./lid/Employee');
 const Manager = require('./lid/Manager');
-const Engineer= require('./lid/Engineer');
-const Intern = require('./lid/Intern'); 
+const Engineer = require('./lid/Engineer');
+const Intern = require('./lid/Intern');
 
-const fs = require('fs'); 
+const fs = require('fs');
 const generatePage = require
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your ID?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid work ID')
+                }
+            }
+        },
+
+    ])
+}
